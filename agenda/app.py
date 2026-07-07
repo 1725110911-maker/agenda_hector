270c95ede4f666a840beb48781bd521c6fb9e98e
HECTOR EMILIANO CRUZ RAMIREZ <1725110911@utectulancingo.edu.mx>
2026-07-07 02:05:27 +0000

    UPDATE agenda

--- agenda/app.py
+++ agenda/app.py
@@ -3,13 +3,14 @@ import web
 urls = (
     '/', 'controllers.index.Index',
     '/lista_contacto', 'controllers.lista_contacto.Lista_contacto',
-    '/ver_contacto', 'controllers.ver_contacto.Ver_contacto',
+    '/ver_contacto/(.*)', 'controllers.ver_contacto.Ver_contacto',
     '/insertar_contacto', 'controllers.insertar_contacto.Insertar_contacto',
-    '/editar_contacto', 'controllers.editar_contacto.Editar_contacto',
-    '/borrar_contacto', 'controllers.borrar_contacto.Borrar_contacto'
+    '/editar_contacto/(.*)', 'controllers.editar_contacto.Editar_contacto',
+    '/borrar_contacto/(.*)', 'controllers.borrar_contacto.Borrar_contacto'
 )
 
 app = web.application(urls, globals())
+
 
 if __name__ == "__main__":
     app.run()
--- agenda/controllers/ver_contacto.py
+++ agenda/controllers/ver_contacto.py
@@ -1,7 +1,47 @@
 import web
+import sqlite3
 
 render = web.template.render('views', base='layout')
 
 class Ver_contacto:
-    def GET(self):
-        return render.ver_contacto()
+
+    def buscarContacto(self, id_contacto):
+        try:
+            # Conecta a la base de datos
+            conn = sqlite3.connect('sql/agenda.db')
+            cursor = conn.cursor()
+            # Consulta los registros de la tabla contactos
+            query = "SELECT * FROM contactos WHERE id_contacto = ?"
+            cursor.execute(query, (id_contacto))            
+            # Crea un array vacio para almacenar los registros
+            contactos = []
+            # Almacena cada registro en un diccionario
+            for row in cursor.fetchall():
+                contacto = {
+                    'id_contacto': row[0],
+                    'nombre': row[1],
+                    'primer_apellido': row[2],
+                    'segundo_apellido': row[3],
+                    'email': row[4],
+                    'telefono': row[5]
+                }
+                # Agrega el diccionario creado al array
+                contactos.append(contacto)
+        
+            # Cierra la conexión a la base de datos
+            conn.close()
+
+            return contactos
+        except sqlite3.Error as error:
+            print(f"ERROR 100: {error.args}")
+            return {}
+        except Exception as error:
+            print(f"ERROR 101: {error.args}")
+            return {  }
+        finally:
+            conn.close()
+
+    def GET(self, id_contacto):
+        print(f"ID_CONTACTO: {id_contacto}")
+        contacto = self.buscarContacto(id_contacto)
+        return render.ver_contacto(contacto)
