ce167653f88a5548d7f9e169fa40bd38537e8dbb
HECTOR EMILIANO CRUZ RAMIREZ <1725110911@utectulancingo.edu.mx>
2026-07-07 02:28:18 +0000

    UPDATE agenda

--- agenda/controllers/ver_contacto.py
+++ agenda/controllers/ver_contacto.py
@@ -10,38 +10,40 @@ class Ver_contacto:
             # Conecta a la base de datos
             conn = sqlite3.connect('sql/agenda.db')
             cursor = conn.cursor()
-            # Consulta los registros de la tabla contactos
+            
+            # Consulta el registro de la tabla contactos
             query = "SELECT * FROM contactos WHERE id_contacto = ?"
-            cursor.execute(query, (id_contacto))            
-            # Crea un array vacio para almacenar los registros
-            contactos = []
-            # Almacena cada registro en un diccionario
-            for row in cursor.fetchall():
+            cursor.execute(query, (id_contacto,))            
+            row = cursor.fetchone()
+            
+            if row:
+                # Si encontró el registro, arma el diccionario
                 contacto = {
                     'id_contacto': row[0],
                     'nombre': row[1],
                     'primer_apellido': row[2],
                     'segundo_apellido': row[3],
-                    'email': row[4],
+                    'email': row[4],  # Verifica que el orden coincida con tus columnas en la BD
                     'telefono': row[5]
                 }
-                # Agrega el diccionario creado al array
-                contactos.append(contacto)
-        
-            # Cierra la conexión a la base de datos
-            conn.close()
+                return contacto # Regresa el diccionario directo
+            else:
+                return {} # Si no hay resultados, regresa un diccionario vacío
 
-            return contactos
         except sqlite3.Error as error:
             print(f"ERROR 100: {error.args}")
             return {}
         except Exception as error:
             print(f"ERROR 101: {error.args}")
-            return {  }
+            return {}
         finally:
-            conn.close()
+            if conn:
+                conn.close()
 
     def GET(self, id_contacto):
         print(f"ID_CONTACTO: {id_contacto}")
         contacto = self.buscarContacto(id_contacto)
         return render.ver_contacto(contacto)
+
+            
+                
